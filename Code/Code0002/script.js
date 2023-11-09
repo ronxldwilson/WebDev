ef9f36da16c883a0d45e0959e7dce6998ecd93e3
ronxldwilson <ronaldwilson00000@gmail.com>
2023-11-09 09:43:56 +0530

    JS practice

---
+++ Code/Code0002/script.js
@@ -0,0 +1,20 @@
+
+console.log(x);
+
+var x = 10;
+
+var hello2 = function hello2(){
+    console.log("hello2");
+}
+
+var hello1 = () =>{
+    console.log("hello1");
+}
+
+function hello(){
+    console.log("Hello");
+}
+
+hello();
+hello1();
+hello2();
