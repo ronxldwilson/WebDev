f14994cfb25b5756368d7d2a636a1e17dc58f8d6
RonxldWilson <ronaldwilson00000@gmail.com>
2024-02-10 20:16:31 +0530

    JS part 2 update

---
+++ Code/Code0004/new.js
@@ -0,0 +1,6 @@
+let y = 5;
+if (true) {
+   // let y = 10; // This creates a new 'y' within the block
+    console.log(y); // Output: 10
+}
+console.log(y);
---
+++ Code/Code0004/quiz.js
@@ -0,0 +1,20 @@
+let name = "Ronald";
+let num = 2;
+
+let newData = name + num ;
+console.log(newData);
+console.log(typeof newData);
+
+const obj = {
+	key : "value",
+	num : 2,
+};
+
+console.log(obj);
+
+obj.age=50;
+
+console.log(obj);
+
+
+
